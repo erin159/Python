4f513c9776c2acbaff6a215db598f1b69079ef7e
Erin <erin3f3n@gmail.com>
2025-05-01 11:54:47 +1200

    AS92004-1.1.py

--- AS92004-1.1.py
+++ AS92004-1.1.py
@@ -1,7 +1,7 @@
 # First name and age
 first_name =input("What is you full name?")
 age =input("How old are you?")
-# if you are between 5 and 17 you can do it, but if you are under 5 you cannot do it, and if you are older than 17 you are to old 
+#if age is between 5 and 17 you can do it but if younger or older then that you are too older or young to do it
 if age < 5:
     print("Sorry you are to young to join the camp")
 if age > 17:
